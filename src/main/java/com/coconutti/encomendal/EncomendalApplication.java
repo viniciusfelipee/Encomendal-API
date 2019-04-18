bb9a517826778c87c4c976f2a64a661cac05907f
Vinicius <viniciusfelipee@hotmail.com.br>
2019-04-18 11:52:31 -0300

    first commit

---
+++ src/main/java/com/coconutti/encomendal/EncomendalApplication.java
@@ -0,0 +1,13 @@
+package com.coconutti.encomendal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncomendalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncomendalApplication.class, args);
+	}
+
+}
